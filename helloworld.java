f8cc2cc5a0d4627e678335e4034ad50fa815d813
unknown <user36@ALCHEMYCLOUD.CO.IN>
2020-07-02 11:42:46 +0530

    add hello world program

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+public class helloworld
+{ 
+ public static void maain(String[] s)
+  {
+    System.out.println("For loop starting- Jenkins execution");
+		for(int i=0;i<=10;i++)
+		{
+		System.out.println(i);
+		}
+		System.out.println("For loop ended- Jenkins execution");
+  }
+ }
+
